aa5e49a47e99bf4e251ffe3602961f3acc7e869c
Brian Moon <brianmoon@MacBook-Pro.local>
2020-01-18 16:11:21 -0500

    first commit

---
+++ src/moon_pa01/GoodbyeWorld.java
@@ -0,0 +1,11 @@
+package moon_pa01;
+
+public class GoodbyeWorld 
+{
+	public static void main(String[] args) 
+	{
+		// printing goodbye world
+		System.out.printf("Goodbye, World!\n");
+	}
+
+}
---
+++ src/moon_pa01/HelloWorld.java
@@ -0,0 +1,11 @@
+package moon_pa01;
+
+public class HelloWorld 
+{
+	public static void main(String[] args) 
+	{
+		// printing hello world
+		System.out.println("Hello, World!");
+	}
+
+}
